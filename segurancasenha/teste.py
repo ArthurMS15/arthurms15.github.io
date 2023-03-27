0e836b040b6c4cd39596ff637de9978ce06c0eb2
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-03-27 13:58:12 -0300

    Create teste.py

---
+++ segurancasenha/teste.py
@@ -0,0 +1,11 @@
+def verificar_senha(senha):
+    #se tiver mais de 8 digitos
+    if len(senha) < 8:
+        return False
+    #verificar maiusculo
+        return False
+    #verificar número
+        return False
+    #verificar se possui caracteres especiais
+        return False
+    return True
