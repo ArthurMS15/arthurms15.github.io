74b92360edd410d6e88ebe74ea2c5e32fbb2db75
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-04-02 15:22:35 -0300

    Levantamento de quais bibliotecas e funções serão utilizadas

--- segurancasenha/teste.py
+++ segurancasenha/teste.py
@@ -1,4 +1,14 @@
 #encriptar uma senha dada pelo usuário, armazenar num banco de dados e depois validar a senha encriptada
+#pip install cryptography
+import re
+import os
+import sqlite3
+from getpass import getpass
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.kdf.concatkdf import ConcatKDFHMAC
+from cryptography.hazmat.backends import default_backend
 
 def verificar_senha(senha):
     #se tiver mais de 8 digitos
