1461dbf8391f10ec0c31f109346c744ca193c88e
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-05-16 22:20:44 -0300

    Update app.py

--- segurancasenha/teste/app.py
+++ segurancasenha/teste/app.py
@@ -1,3 +1,4 @@
+#aplicativo web simples
 from flask import Flask, render_template, request, redirect, url_for
 from senhateste import validar_senha, enviar_email_alerta, salvar_senha_db, gerar_chave
 import socket
@@ -18,11 +19,15 @@ def registro():
     if request.method == 'POST':
         email = request.form['email']
         senha = request.form['senha']
-        salt = os.urandom(16)
+
+        salt = os.urandom(16) #numero de bytes aleatórios
         fernet = gerar_chave(senha, salt)
         senha_encriptada = fernet.encrypt(senha.encode())
+        
+        #Cria um salt aleatório, usa o salt e a senha para gerar uma chave de criptografia e, em seguida, usa essa chave para criptografar a senha.
         salvar_senha_db(email, senha_encriptada, salt)
         return redirect(url_for('login'))
+        #Salva o e-mail, a senha criptografada e o salt no banco de dados e, em seguida, redireciona o usuário para a página de login.
     return render_template('registro.html')
 
 @app.route('/verificar', methods=['POST'])
@@ -44,10 +49,8 @@ def verificar():
             failed_attempts += 1
             if failed_attempts >= 3:
                 failed_attempts = 0
-                ip = socket.gethostbyname(socket.gethostname())
+                ip = socket.gethostbyname(socket.gethostname()) #obter endereço do host
                 enviar_email_alerta(email, ip, tentativa_acesso=True)  # Função para enviar e-mail de alerta
-                return render_template('registro.html')  # Página de alerta de força bruta
-            else:
                 return render_template('login.html', error=f"Senha incorreta! Tentativas restantes: {3 - failed_attempts}")
 
 if __name__ == '__main__':
